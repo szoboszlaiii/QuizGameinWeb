60f317fd0d7605468ea740b3d4cf83303d4b04fb
IFSZNB23\sbo <szistvan2014@gmail.com>
2022-09-11 17:59:21 +0200

    Question controller megírása getall függvényre

---
+++ Web Api/CompanyEmployees/CompanyEmployees/Controllers/QuestionController.cs
@@ -0,0 +1,43 @@
+﻿using AutoMapper;
+using CompanyEmployees.Contracts;
+using CompanyEmployees.Entities.DataTransferObjects;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CompanyEmployees.Controllers
+{
+    [Route("api/questions")]
+    [Authorize]
+    [ApiController]
+    public class QuestionController : ControllerBase
+    {
+        private readonly IRepositoryManager _repository;
+        private readonly IMapper _mapper;
+
+        public QuestionController(IRepositoryManager repository, IMapper mapper)
+        {
+            _repository = repository;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        [Route("getquestions")]
+        public IActionResult GetQuestions()
+        {
+            try
+            {
+                var claims = User.Claims;
+
+                var question=_repository.Question.GetAllQuestion(trackChanges: false);
+
+                var questionsDto = _mapper.Map<IEnumerable<QuestionDto>>(question);
+
+                return Ok(questionsDto);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, "Internal server error");
+            }
+        }
+    }
+}
