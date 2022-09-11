3525c46b84254ee38d334112ef6156f051ccfbe0
IFSZNB23\sbo <szistvan2014@gmail.com>
2022-09-11 17:56:23 +0200

    Question Repository létrehozása

---
+++ Web Api/CompanyEmployees/CompanyEmployees/Repository/QuestionRepository.cs
@@ -0,0 +1,18 @@
+﻿using CompanyEmployees.Contracts;
+using CompanyEmployees.Entities.Models;
+
+namespace CompanyEmployees.Repository
+{
+    public class QuestionRepository : RepositoryBase<Questions_v>, IQuestionRepository
+    {
+        public QuestionRepository(RepositoryContext repositoryContext)
+            : base(repositoryContext)
+        {
+        }
+
+        public IEnumerable<Questions_v> GetAllQuestion(bool trackChanges) =>
+           FindAll(trackChanges)
+           .OrderBy(c => c.Questions)
+           .ToList();
+    }
+}
