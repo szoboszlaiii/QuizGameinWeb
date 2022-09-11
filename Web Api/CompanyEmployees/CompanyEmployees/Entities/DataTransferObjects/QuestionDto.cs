52e0a411c0b67ba94f41c1c77601a9820f18a36b
IFSZNB23\sbo <szistvan2014@gmail.com>
2022-09-11 17:56:55 +0200

    Model és dto létrehozása a kérdéseknek

---
+++ Web Api/CompanyEmployees/CompanyEmployees/Entities/DataTransferObjects/QuestionDto.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CompanyEmployees.Entities.DataTransferObjects
+{
+    public class QuestionDto
+    {
+            public string? Questions { get; set; }
+            public string? Answer1 { get; set; }
+            public string? Answer2 { get; set; }
+            public string? Answer3 { get; set; }
+            public string? Answer4 { get; set; }
+            public string? Correct_Answer { get; set; }
+    }
+}
---
+++ Web Api/CompanyEmployees/CompanyEmployees/Entities/Models/Question.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CompanyEmployees.Entities.Models
+{
+    [Keyless]
+    public class Questions_v
+    {
+        [Column("questions")]
+        public string? Questions { get; set; }
+
+        [Column("answer1")]
+        public string? Answer1 { get; set; }
+
+        [Column("answer2")]
+        public string? Answer2 { get; set; }
+
+        [Column("answer3")]
+        public string? Answer3 { get; set; }
+
+        [Column("answer4")]
+        public string? Answer4 { get; set; }
+
+        [Column("correct_answer")]
+        public string? Correct_Answer { get; set; }
+    }
+}
